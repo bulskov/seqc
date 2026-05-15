cff16b0a46db4d8ea7fc7d57073d2ad6c03fa502
bulskov <bulskov@ruc.dk>
2026-05-15 23:27:49 +0200

    Add clean option to build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
-PRESET=${1:-debug}
+CLEAN=0
+PRESET="debug"
+
+for arg in "$@"; do
+    case "$arg" in
+        clean) CLEAN=1 ;;
+        *)     PRESET="$arg" ;;
+    esac
+done
+
+if [[ $CLEAN -eq 1 ]]; then
+    rm -rf "build/$PRESET"
+fi
 
 cmake --preset "$PRESET"
 cmake --build --preset "$PRESET"
